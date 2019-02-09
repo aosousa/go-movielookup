c94e99099234f55c6a443eefaaffc9a8bbbafe19
André Sousa <andreosousa95@gmail.com>
2019-02-09 13:58:21 -0100

    Updated main.go after new utils package

--- main.go
+++ main.go
@@ -8,14 +8,14 @@ import (
 
 func main() {
 	// set up Config struct
-	utils.initConfig()
+	utils.InitConfig()
 
 	// return help message if length of command line arguments is 1
 	// we pick 1 instead of 0 because it'll never be 0
 	// since os.Args[0] returns the name of the file being executed (in this case, go-movie-lookup.exe)
 	args := os.Args
 	if len(args) == 1 {
-		utils.printHelp()
+		utils.PrintHelp()
 		return
 	}
 
@@ -23,14 +23,14 @@ func main() {
 
 	switch cmd {
 	case "-m", "--movie":
-		utils.handleMovie(args)
+		utils.HandleMovie(args)
 	case "-s", "--show":
-		utils.handleShowOptions(args)
+		utils.HandleShowOptions(args)
 	case "-h", "--help":
-		utils.printHelp()
+		utils.PrintHelp()
 	case "-v", "--version":
-		utils.printVersion()
+		utils.PrintVersion()
 	default:
-		utils.printHelp()
+		utils.PrintHelp()
 	}
 }
